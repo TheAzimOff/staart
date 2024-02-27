1981e077f2c4c251fb07e78de97b06d95daee632
Izzatillo Azimov <croosdev@gmail.com>
2024-02-27 22:03:39 +0500

    build routes and sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link';
+import React from 'react';
+import { FaHackerrank } from 'react-icons/fa';
+import {
+  LuFilm,
+  LuHome,
+  LuImage,
+  LuNewspaper,
+  LuSettings,
+} from 'react-icons/lu';
+
+const Sidebar = () => {
+  return (
+    <nav className='text-3xl h-screen fixed top-0 left-0 flex justify-between flex-col py-8 px-4'>
+      <div className='home '>
+        <Link href='/'>
+          <LuHome />
+        </Link>
+      </div>
+      <ul className='flex flex-col gap-6'>
+        <li className='menuItem '>
+          <Link href='/news'>
+            <LuNewspaper />
+          </Link>
+        </li>
+        <li className='menuItem'>
+          <Link href='/hackerrank'>
+            <FaHackerrank />
+          </Link>
+        </li>
+        <li className='menuItem'>
+          <Link href='/movies'>
+            <LuFilm />
+          </Link>
+        </li>
+        <li className='menuItem'>
+          <Link href='/images'>
+            <LuImage />
+          </Link>
+        </li>
+      </ul>
+      <div className='settings'>
+        <LuSettings />
+      </div>
+    </nav>
+  );
+};
+
+export default Sidebar;
---
+++ app/hackerrank/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Hackerrank = () => {
+  return <div>Hackerrank</div>;
+};
+
+export default Hackerrank;
